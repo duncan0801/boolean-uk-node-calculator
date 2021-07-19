f9af943e0f809a1c77aa316d00bafb0a26211eb3
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-07-19 16:57:01 +0100

    Completed main task

--- script.js
+++ script.js
@@ -3,11 +3,9 @@
 function parseArgs(argsArray) {
     return argsArray.slice(2)
 }
-
 const parsedArgs = parseArgs(process.argv)
-
 const numberA = parsedArgs[0]
-const operand = parsedArgs[1]
+const operator = parsedArgs[1]
 const numberB = parsedArgs[2]
 
 const performCalc = {
@@ -16,6 +14,20 @@ const performCalc = {
     "+": numberA + numberB,
     "-": numberA - numberB
 }
-let answer = performCalc[operand]
 
-console.log(`${numberA} ${operand} ${numberB} = ${answer}`)
+const numberAType = Number(numberA)
+const numberBType = Number(numberB)
+
+const acceptedOperators = Object.keys(performCalc)
+
+if (!acceptedOperators.includes(operator)) {
+    console.log("That is not an acceptable operator. Choose either \"*\",\"/\", \"+\", \"-\"")
+}
+
+if(isNaN(numberAType) || isNaN(numberBType)) {
+    console.log("One of your inputs is not a number. Please enter any number.")
+} 
+else {
+    let answer = performCalc[operator]
+    console.log(`${numberA} ${operator} ${numberB} = ${answer}`)
+}
